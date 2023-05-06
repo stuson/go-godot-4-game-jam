60a813529029348e7d5205dfcb181f9373cb99e7
Daniel Bardsley <danwbardsley@gmail.com>
2023-05-06 17:12:35 +0100

    implement spawn groups

--- stages/Spawn.gd
+++ stages/Spawn.gd
@@ -2,4 +2,19 @@ extends Node
 class_name Spawn
 
 export(PackedScene) var Enemy
-export(int) var num_enemies
+export(int) var num_enemies = 1
+export var spawn_interval: float = 0.1
+var time_since_spawn: float
+var remaining_enemies: int
+
+func _ready():
+	remaining_enemies = num_enemies
+
+func get_spawn_count(delta: float) -> int:
+	time_since_spawn += delta
+	var spawn_count = 0
+	while time_since_spawn > spawn_interval and remaining_enemies > 0:
+		spawn_count += 1
+		remaining_enemies -= 1
+		time_since_spawn -= spawn_interval
+	return spawn_count
--- stages/SpawnManager.gd
+++ stages/SpawnManager.gd
@@ -6,8 +6,15 @@ onready var wave_delay_timer = $WaveDelayTimer
 
 var current_wave_idx = -1
 var current_wave: Wave
-var enemies_to_spawn_remaining
+var spawn_groups: Array
+var current_group_idx = -1
+var current_group: SpawnGroup
+var spawns: Array
+var enemies_in_wave = 0
+var enemies_in_group_to_spawn = 0
+var enemies_spawned = 0
 var enemies_killed = 0
+var cum_delta
 
 signal new_wave_spawned
 signal enemies_to_kill_updated
@@ -21,24 +28,45 @@ func trigger_next_wave() -> void:
 
 func spawn_next_wave():
 	enemies_killed = 0
+	enemies_spawned = 0
 	current_wave_idx += 1
+	spawn_timer.stop()
 	if current_wave_idx < waves.size():
 		current_wave = waves[current_wave_idx]
-		enemies_to_spawn_remaining = current_wave.num_enemies
-		spawn_timer.wait_time = current_wave.spawn_interval
-		spawn_timer.start()
-		emit_signal("new_wave_spawned", current_wave_idx, current_wave.num_enemies)
+		spawn_groups = current_wave.get_children()
+		
+		enemies_in_wave = 0
+		for group in spawn_groups:
+			spawns = group.get_children()
+			for spawn in spawns:
+				enemies_in_wave += spawn.num_enemies
+		
+		current_group_idx = -1
+		get_next_spawn_group()
+		
+		emit_signal("new_wave_spawned", current_wave_idx, enemies_in_wave)
+
+func _process(delta: float) -> void:
+	if current_wave:
+		enemies_in_group_to_spawn = 0
+		for spawn in spawns:
+			var spawn_count = spawn.get_spawn_count(delta)
+			enemies_in_group_to_spawn += spawn.remaining_enemies
+			for idx in range(0, spawn_count):
+				var enemy: Node2D = spawn.Enemy.instance()
+				var stats = enemy.get_node("Stats")
+				stats.connect("die", self, "_on_Enemy_die")
+				get_tree().current_scene.add_child(enemy)
+				enemy.global_translate(get_spawn_pos())
+				enemies_spawned += 1
+		if enemies_in_group_to_spawn <= 0 and spawn_timer.is_stopped():
+			spawn_timer.wait_time = current_group.spawn_duration
+			spawn_timer.start()
+		
 
 func _on_SpawnTimer_timeout() -> void:
-	if enemies_to_spawn_remaining <= 0:
-		spawn_timer.stop()
-	else:
-		var enemy: Node2D = current_wave.Enemy.instance()
-		var stats = enemy.get_node("Stats")
-		stats.connect("die", self, "_on_Enemy_die")
-		get_tree().current_scene.add_child(enemy)
-		enemy.global_translate(get_spawn_pos())
-		enemies_to_spawn_remaining -= 1
+	spawn_timer.stop()
+	get_next_spawn_group()
 
 func get_spawn_pos() -> Vector2:
 	randomize()
@@ -56,12 +84,20 @@ func get_spawn_pos() -> Vector2:
 			return Vector2(rand_range(-5, view.size.x + 5), view.size.y + 5)
 		_:
 			return Vector2(-5, -5)
-	
+
+func get_next_spawn_group():
+	current_group_idx += 1
+	if current_group_idx < spawn_groups.size():
+		current_group = spawn_groups[current_group_idx]
+		spawns = current_group.get_children()
+
 func _on_Enemy_die() -> void:
 	enemies_killed += 1
-	emit_signal("enemies_to_kill_updated", current_wave.num_enemies - enemies_killed)
+	emit_signal("enemies_to_kill_updated", enemies_in_wave - enemies_killed)
 	
-	if enemies_killed >= current_wave.num_enemies:
+	if enemies_killed >= enemies_spawned and not spawn_timer.is_stopped():
+		spawn_timer.wait_time = min(spawn_timer.time_left, 1)
+	if enemies_killed >= enemies_in_wave:
 		emit_signal("wave_cleared")
 
 
