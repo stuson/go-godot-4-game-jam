e2c5d4ee88eba134cfe40163d185adbfbb39c8a6
Daniel Bardsley <danwbardsley@gmail.com>
2023-05-06 13:30:04 +0100

    add basic folder structuring

--- characters/Stats.gd
+++ characters/Stats.gd
@@ -10,9 +10,9 @@ signal hp_changed
 signal die
 
 func take_hit(damage) -> void:
-    current_hp -= damage
-    emit_signal("hp_changed", current_hp, max_hp)
-    
-    if current_hp <= 0:
-        emit_signal("die")
-    
+	current_hp -= damage
+	emit_signal("hp_changed", current_hp, max_hp)
+	
+	if current_hp <= 0:
+		emit_signal("die")
+	
