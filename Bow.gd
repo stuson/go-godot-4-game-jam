19269a2c19e12b82643b40990eccb9701c4b278d
Sam Tuson <sam.tuson@hotmail.co.uk>
2023-05-06 13:54:13 +0100

    Add upgrade

--- Bow.gd
+++ Bow.gd
@@ -11,16 +11,16 @@ export(PackedScene) var Projectile
 
 
 func attack() -> void:
-	if can_shoot:
-		can_shoot = false
-		
-		arrow = Projectile.instance()
-		arrow.global_transform = global_transform
-		arrow.damage = BASE_DAMAGE
-		get_tree().current_scene.add_child(arrow)
-		
-		rof_timer.start()
+    if can_shoot:
+        can_shoot = false
+        
+        arrow = Projectile.instance()
+        arrow.global_transform = global_transform
+        arrow.damage = BASE_DAMAGE
+        get_tree().current_scene.add_child(arrow)
+        
+        rof_timer.start()
 
 
 func _on_Timer_timeout() -> void:
-	can_shoot = true
+    can_shoot = true
---
+++ MoreFood.gd
@@ -0,0 +1,13 @@
+extends Button
+
+onready var player: Player = get_tree().get_nodes_in_group("Player")[0]
+onready var player_stats: Stats = player.get_node("Stats")
+
+signal upgrade_selected
+
+func _on_MoreFood_pressed() -> void:
+    player_stats.max_hp += 1
+    player_stats.current_hp = player_stats.max_hp
+    player_stats.emit_hp_change()
+    
+    emit_signal("upgrade_selected")
