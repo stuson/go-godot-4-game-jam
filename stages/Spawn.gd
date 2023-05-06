f0299a546fc977d865ea8eb4a938d700162fde18
Daniel Bardsley <danwbardsley@gmail.com>
2023-05-06 15:51:21 +0100

    add Spawn & SpawnGroup classes

---
+++ stages/Spawn.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name Spawn
+
+export(PackedScene) var Enemy
+export(int) var num_enemies
---
+++ stages/SpawnGroup.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name SpawnGroup
+
+export var spawn_duration: float = 1
