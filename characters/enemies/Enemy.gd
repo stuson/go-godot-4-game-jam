25f37fc91a1f2fcfad1dc891b57c4326181aacc9
Daniel Bardsley <danwbardsley@gmail.com>
2023-05-08 13:34:33 +0100

    add general death explosion logic

--- characters/enemies/Enemy.gd
+++ characters/enemies/Enemy.gd
@@ -5,6 +5,8 @@ class_name Enemy
 export var damage = 1
 export var max_hp = 5
 export var speed = 100
+export(Color) var explode_color
+export(float) var explode_scale = 1
 
 var velocity
 var knockback_velocity = Vector2.ZERO
@@ -14,9 +16,12 @@ onready var stats = $Stats
 onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 onready var nav_timer = $NavigationRefreshTimer
 
+export(PackedScene) var death_explosion
+
 func _ready() -> void:
     stats.max_hp = max_hp
     stats.current_hp = max_hp
+    stats.move_speed = speed
     stats.connect("die", self, "_on_Stats_die")
     nav_timer.connect("timeout", self, "_on_NavigationRefreshTimer_timeout")
     
@@ -24,7 +29,7 @@ func _physics_process(delta: float) -> void:
     if not nav_agent.is_navigation_finished():
         var target_pos = nav_agent.get_next_location()
         var direction = global_transform.origin.direction_to(target_pos)
-        velocity = direction * speed
+        velocity = direction * stats.move_speed
         velocity += get_knockback_velocity()
         move_and_slide(velocity)
 
@@ -37,7 +42,13 @@ func get_knockback_velocity() -> Vector2:
     return knockback_velocity
 
 func _on_Stats_die() -> void:
-    queue_free()
+    var explosion = death_explosion.instance()
+    explosion.color = explode_color
+    explosion.death_action = funcref(self, "death_explosion")
+    explosion.global_position = global_position
+    explosion.scale *= explode_scale * sqrt(scale.length())
+    get_tree().current_scene.add_child(explosion)
+    
 
 func _on_Area2D_body_entered(body: Node) -> void:
     if body.has_node("Stats"):
@@ -45,3 +56,12 @@ func _on_Area2D_body_entered(body: Node) -> void:
 
 func _on_NavigationRefreshTimer_timeout() -> void:
     nav_agent.set_target_location(player.global_position)
+
+func death_explosion(bodies: Array) -> void:
+    for body in bodies:
+        if body.has_node("Stats"):
+            death_explosion_effect(body)
+    queue_free()
+    
+func death_explosion_effect(body) -> void:
+    pass #Implement per enemy
--- characters/enemies/tank_enemy/TankEnemy.gd
+++ characters/enemies/tank_enemy/TankEnemy.gd
@@ -1,1 +1,8 @@
 extends Enemy
+
+func death_explosion_effect(body: KinematicBody2D) -> void:
+    var stats: Stats = body.get_node("Stats")
+    var heal = round(stats.max_hp * 0.3)
+    stats.max_hp = round(stats.max_hp * 1.3)
+    stats.current_hp += heal
+    body.scale *= 1.3
