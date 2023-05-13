b2d0e939b7d9192957c81d0f85b798aca57f28c5
Sam Tuson <sam.tuson@hotmail.co.uk>
2023-05-13 14:05:11 +0100

    Swing tweaks

--- characters/player/weapons/sword/Swing.gd
+++ characters/player/weapons/sword/Swing.gd
@@ -4,26 +4,28 @@ onready var life_timer = $LifeTimer
 onready var fade_out = $FadeOut
 onready var swing_area = $SwingArea
 onready var sfx_player = $SfxPlayer
+onready var animation_player = $FadeOut
 
 export(Array, AudioStream) var swoosh_sfx
 
 var damage
 var knockback_multiplier
 var is_crit
+var hit_objects = []
 
 func _ready() -> void:
-    life_timer.start()
     sfx_player.pitch_scale = rand_range(0.9, 1.1)
     sfx_player.stream = swoosh_sfx[randi() % swoosh_sfx.size()]
     sfx_player.play()
-       
-func _on_LifeTimer_timeout() -> void:
-    swing_area.monitoring = false
     fade_out.play("Fade Out")
 
 func _on_SwingArea_body_entered(body: Node) -> void:
     if body.has_node("Stats"):
-        body.take_hit(damage, (body.global_position - global_position).normalized(), knockback_multiplier, is_crit)
-
+        var id = body.get_instance_id()
+        
+        if not id in hit_objects:
+            body.take_hit(damage, (body.global_position - global_position).normalized(), knockback_multiplier, is_crit)
+            hit_objects.append(id)
+        
 func _on_FadeOut_animation_finished(anim_name):
     queue_free()
