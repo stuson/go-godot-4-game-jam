434dc3ee079da8484f44609aa1fb852a8155a0b4
Daniel Bardsley <danwbardsley@gmail.com>
2023-05-14 20:01:54 +0100

    animate healing potion

--- items/healing_potion/HealingPotion.gd
+++ items/healing_potion/HealingPotion.gd
@@ -3,7 +3,17 @@ extends Node2D
 const potion = true
 const healing = 30
 
+onready var animation_player = $AnimationPlayer
+
+func _ready() -> void:
+    animation_player.play("Fade Out")
+
 func _on_Area2D_area_entered(area):
     var player = area.get_parent()
     player.stats.current_hp += healing
+    queue_free()
+
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
     queue_free()
