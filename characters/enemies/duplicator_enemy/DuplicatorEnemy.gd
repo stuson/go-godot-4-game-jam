933a37a13879a0b3ab8e4a683d38775caf68fb6e
Daniel Bardsley <danwbardsley@gmail.com>
2023-05-14 22:43:32 +0100

    add duplicated enemies to ysort

--- characters/enemies/duplicator_enemy/DuplicatorEnemy.gd
+++ characters/enemies/duplicator_enemy/DuplicatorEnemy.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 func death_explosion_effect(body: KinematicBody2D) -> void:
     if body.duplicatable:
         var dupe = body.clone()
-        get_tree().current_scene.call_deferred("add_child", dupe)
+        ysort.call_deferred("add_child", dupe)
         randomize()
         dupe.global_position = body.global_position + Vector2(randi()%41-20, randi()%41-20)
